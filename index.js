f23367351fa96d3fc062069c6e7b5745ab7f0354
Nate Geslin <ngeslin@nerdery.com>
2018-06-03 21:26:52 -0500

    Initialize

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+const minimist = require('minimist');
+const notifier = require('./lib/notifier');
+const airportValidator = require('./lib/airportValidator');
+
+const OPTIONS = {
+    _fullPathToAirportFile: '',
+    airport: '',
+    shouldValidateFixes: false,
+    shouldValidateSpawnPatterns: false
+};
+
+//
+// -a ksea             entire airport file
+// --fixes             fixes, valid position and references
+// --spawn-patterns    spawnPatterns, valid fixenames and object shapes
+
+function _parseArgs() {
+    const args = minimist(process.argv.slice(2));
+
+    if (typeof args.a !== 'undefined') {
+        OPTIONS.airport = args.a;
+        OPTIONS._fullPathToAirportFile = path.join(__dirname, OPTIONS.airport);
+    }
+
+    if (typeof args.shouldValidateFixes !== 'undefined') {
+        OPTIONS.shouldValidateFixes = args.fixes;
+    }
+
+    if (typeof args.shouldValidateSpawnPatterns !== 'undefined') {
+        OPTIONS.shouldValidateFixes = args.spawnPatterns;
+    }
+}
+
+function _loadAirportFile(args) {
+    fs.readFile(OPTIONS._fullPathToAirportFile, 'utf8', (err, airportJson) => {
+        if (err) {
+            notifier.fail();
+        }
+
+        notifier.succeed();
+
+        airportValidator(JSON.parse(airportJson), OPTIONS);
+    });
+}
+
+(function() {
+    notifier.start({ text: 'Parsing ARGS' });
+
+    // parse args
+    _parseArgs();
+
+    notifier.succeed();
+    notifier.start({ text: `Loading Airport file: ${OPTIONS.airport}`});
+
+    // open airport file
+    _loadAirportFile();
+})();
