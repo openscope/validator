66e37bd4c1c07c35c5e67054b8917b77922a9c29
Nate Geslin <ngeslin@nerdery.com>
2018-06-16 23:24:11 -0500

    adds initial documentation in .parseArgs() and BaseValidator

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const parseArgs = require('./lib/parseArgs');
 const validator = require('./lib/validator');
 
 /**
+ * References to files and paths that can be used elsewhere in the app
  *
- * @param {object} options
+ * @constant
+ * @property PATHS
+ */
+const PATHS = {
+    AIRPORT_DIR: './assets/airports',
+    AIRCRAFT: './public/assets/aircraft/aircraft.json',
+    AIRLINES: './public/assets/airlines/airlines.json'
+};
+
+/**
+ *
+ *
+ * @private
+ * @function _loadAirportFile
+ * @param {CliOptionsType} options
  */
 function _loadAirportFile(options) {
     Notifier.start(`Loading Airport file: ${options.airport}`);
+
     fs.readFile(options.fullPathToAirportFile, 'utf8', (err, airportJson) => {
         if (err) {
             Notifier.fail();
@@ -23,7 +39,7 @@ function _loadAirportFile(options) {
 }
 
 (function() {
-    const options = parseArgs();
+    const options = parseArgs(PATHS);
 
     if (options.shouldExit) {
         console.log('\r\n\r\n help docs here');
